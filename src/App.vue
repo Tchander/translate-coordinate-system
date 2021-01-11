e5143d55b958a9b7024f646cb9064a61aa99cda0
Tchander <evgeniy.tchander@yandex.ru>
2021-01-11 18:32:50 +0300

    Start to creating TCS on vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TranslateCoordinateToXYZ />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TranslateCoordinateToXYZ from "@/components/TranslateCoordinateToXYZ";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TranslateCoordinateToXYZ
   }
 }
 </script>
---
+++ src/components/TranslateCoordinateToXYZ.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Translate coordinate system to XYZ</h1>
+  </div>
+</template>
+
+<script>
+ export default {
+
+ }
+</script>
